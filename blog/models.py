63618f149db20c9a120a96ba04ecd24ce1da70dd
JJ <jedrzej@roomsage.com>
2020-11-05 16:22:35 +0100

    polished the portfolio website, added singular blogs

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,17 @@ class Blog(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
     image = models.ImageField(upload_to="images/")
+
+    def summary(self):
+        str_body = str(self.body)
+        if len(str_body.split()) < 10:
+            return str_body
+        else:
+            return " ".join(str_body.split()[:10]) + "..."
+
+    def pub_date_pretty(self):
+        print(self.pub_date)
+        return self.pub_date
+
+    def __str__(self):
+        return self.title
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Job
 
+
 def home(request):
 
     jobs = Job.objects
